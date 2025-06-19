11850d1c2d61962fe567b4e289df8be9d042ef54
Rfinity <11379456+Rfinity@users.noreply.github.com>
2025-06-19 15:26:41 -0500

    That fixes it? wow

--- src/components/login/LoginComponent.jsx
+++ src/components/login/LoginComponent.jsx
@@ -4,6 +4,7 @@ import { isLoggedIn } from "./state";
 const LoginComponent = (props) => {
   const $isLoggedIn = useStore(isLoggedIn);
   console.log("LoginComponent render:\n $isLoggedIn:", $isLoggedIn, "\n typeof profileIndicator:", typeof profileIndicator, "\n typeof loginButton:", typeof loginButton);
+  console.log("props,", props);
   return (
     <div className="absolute top-8 right-8 z-10">
       {$isLoggedIn ? (props.profileIndicator ?? "Profile Indicator Fallback") : (props.loginButton ?? "Login Button Fallback")}
